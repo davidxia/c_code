ad8d0a18455d48c1e57984c8ef538e93d907e230
David Xia <david@davidxia.com>
2013-01-07 19:42:01 -0500

    Onto LZ77 compression in mastering algos with C

---
+++ algos_with_c/ch14/transfer.h
@@ -0,0 +1,2 @@
+int send_comp(int s, const unsigned char *data, int size, int flags);
+int recv_comp(int s, unsigned char **data, int *size, int flags);
---
+++ endian_test.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+#define BIG_ENDIAN 0
+#define LITTLE_ENDIAN 1
+
+
+int endian() {
+    short int word = 0x0001;
+    char *byte = (char *) &word;
+    return (byte[0] ? LITTLE_ENDIAN : BIG_ENDIAN);
+}
+
+
+int main() {
+    int value;
+    value = endian();
+    if (value == 1)
+        printf("Machine is little endian\n", value);
+    else
+        printf("machine is big endian\n", value);
+}
+
